b894176a667dab958129df45f68ee1df28cee4e6
Brett Huffman <dicer2000@yahoo.com>
2021-02-10 20:43:24 -0600

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,46 @@
+# master Application
+
+master application by Brett Huffman for CMP SCI 4760 - Project 2 - V1.0
+
+This program implements the master program...
+
+A log was kept of each day's activity.  It is found at the end of this README file.
+
+A Git repository was maintained with a public remote found here: https://github.com/dicer2000/CS4760_Master.git
+
+## Program Switches
+The program can be invoked as:
+
+```
+master [-h]
+master [-h] [-s i] [-t time] datafile
+Options:
+  -h        Describe how the project should be run and then, terminate.
+  -s x      Indicate the number of children allowed to exist in the system at the same time. (Default 20)
+  -t time   The time in seconds after which the process will terminate, even if it has not finished. (Default 100)
+  datafile  Input file containing one integer on each line.
+  
+```
+
+## Install
+To install this program, clone it with git to the folder to which you want 
+it saved.
+```
+git clone https://github.com/dicer2000/CS4760_Master.git
+```
+## Compile
+To compile the mydu application, simply run the make command:
+```
+make
+```
+## Run
+To run the program, use the master command.  You can use any of the attributes listed in program switches area.
+
+## Problems / Issues
+
+
+## Work Log
+
+- 2/10/2021 - Created project, Makefile, this readme file and got it all to compile
+
+*©2021 Brett W. Huffman*
---
+++ master.cpp
@@ -0,0 +1,77 @@
+/********************************************
+ * doenv - A env derivation program
+ * Brett Huffman
+ * CMP SCI 4760 - Project 1
+ * Due Feb 9, 2021
+ * Main CPP file for project
+ ********************************************/
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+// Forward declarations
+static void show_usage(std::string);
+void printEnvironment();
+
+// Main - expecting arguments
+int main(int argc, char* argv[])
+{
+    // Argument processing
+    int opt;
+
+    // Go through each parameter entered and
+    // prepare for processing
+    while ((opt = getopt(argc, argv, "hst")) != -1) {
+        switch (opt) {
+            case 'h':
+                show_usage(argv[0]);
+                return 0;
+            case 's':
+                break;
+            case 't':
+                break;
+            case '?': // Unknown arguement                
+                if (isprint (optopt))
+                {
+                    errno = EINVAL;
+                    perror("Unknown option");
+//                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                }
+                else
+                {
+                    errno = EINVAL;
+                    perror("Unknown option character");
+                }
+                return -1;
+            default:    // An bad input parameter was entered
+                // Show error because a bad option was found
+                perror ("master: Error: Illegal option found");
+                show_usage(argv[0]);
+                return -1;
+        }
+    }
+
+    return 0;
+}
+
+
+// Handle errors in input arguments by showing usage screen
+static void show_usage(std::string name)
+{
+    std::cerr << std::endl
+              << name << " - master app by Brett Huffman for CMP SCI 4760" << std::endl
+              << std::endl
+              << "Usage:\t" << name << " [-h]" << std::endl
+              << "\t" << name << " [-i] [var1=value] [var2=value] [...] {command1 [;command2] [;...]}" << std::endl
+              << "Options:" << std::endl
+              << "  -h        Describe how the project should be run and then, terminate." << std::endl
+              << "  -s x      Indicate the number of children allowed to exist in the system at the same time. (Default 20)" << std::endl
+              << "  -t time   The time in seconds after which the process will terminate, even if it has not finished. (Default 100)"
+              << "  datafile  Input file containing one integer on each line."
+              << std::endl << std::endl;
+}
